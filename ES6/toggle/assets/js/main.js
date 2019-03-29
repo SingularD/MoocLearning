4225286a532a785c00783ac4f696fae2e60b6c7f
SingularD <906229740@qq.com>
2019-03-29 23:28:52 +0800

    toggle demo

--- ES6/toggle/assets/js/main.js
+++ ES6/toggle/assets/js/main.js
@@ -14,6 +14,7 @@ const methods = {
 class CustomSelect {
   constructor(data) {
     this.categories = {};
+    this.liList = []
     this._init(data); // 初始化数据
     this._createElement(); // 生成元素
     this._bind(); // 绑定事件
@@ -27,13 +28,36 @@ class CustomSelect {
       }
       this.categories[type].push(value)
     })
-    console.log(this.categories)
   }
 
   _createElement() {
+    let selectOptions = methods.$('.select-options')
+    for (let key of Object.keys(this.categories)) {
 
+      if (!this.liList.includes(key)) {
+
+        let ul = document.createElement('ul')
+        ul.className = 'options'
+
+        let liArr = []
+        this.categories[key].forEach((value) => {
+          liArr.push(`<li class="options-item">${data[parseInt(value) - 1].text}</li>`)
+        })
+
+        ul.innerHTML = `<li class="options-item options-title"><h3>${key}</h3></li>${liArr.join('')}`
+
+        methods.appendChild(selectOptions, ul)
+
+      }
+    }
+    console.log(methods.$$('.options-title'))
+    methods.$$('.options-title').forEach( item => {
+      item.style.marginLeft = '0px'
+    })
+    methods.$$('.options-item').forEach( item => {
+      item.style.marginLeft = '30px'
+    })
   }
-
   _bind() {
 
   }
