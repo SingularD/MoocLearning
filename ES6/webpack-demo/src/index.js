ee7f8fc3b5bb1c116e386ee5190f7dca59962d7c
SingularD <906229740@qq.com>
2019-04-03 12:06:00 +0800

    webpack

--- ES6/webpack-demo/src/index.js
+++ ES6/webpack-demo/src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.jpg'
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['hello', 'webpack'], '');
+  element.classList.add('hello');
 
-  return element
+  let myIcon = new Image();
+  myIcon.src = Icon;
+  console.log(Icon);
+  element.appendChild(myIcon);
+
+  return element;
 }
+
 
 document.body.appendChild(component());
