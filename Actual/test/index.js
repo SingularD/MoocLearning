0b60b89a10f332e775ae56271fd302462c9ebe6a
SingularD <906229740@qq.com>
2019-04-22 23:04:15 +0800

    前端面试

--- Actual/test/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-  app.listen(4444, function() {
-    console.log('App listening at port 8080;');
-  });
